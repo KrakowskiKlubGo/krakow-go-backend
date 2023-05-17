825e3c34b0e0b6e5cc701e068e46f994657b46de
jmilkowski <jmilkowski@i2asolutions.com>
2023-05-17 17:01:03 +0200

    add migrations, remove MeetingParticipant from admin

--- meetings/admin.py
+++ meetings/admin.py
@@ -2,26 +2,16 @@ from django.contrib import admin
 
 from common.admin import CloneTranslationModelAdmin
 from meetings.models import (
-    MeetingParticipant,
-    Meeting,
     OneTimeMeeting,
     RecurringMeeting,
 )
 
 
-class MeetingParticipantInlinie(admin.TabularInline):
-    fk_name = "meeting"
-    model = MeetingParticipant
-    extra = 0
-
-
 @admin.register(OneTimeMeeting)
 class MeetingAdmin(CloneTranslationModelAdmin):
     list_display = ("name_pl", "date", "start_time", "end_time")
-    inlines = [MeetingParticipantInlinie]
 
 
 @admin.register(RecurringMeeting)
 class RecurringMeeting(CloneTranslationModelAdmin):
     list_display = ("name_pl", "day_of_week", "start_time", "end_time")
-    inlines = [MeetingParticipantInlinie]
