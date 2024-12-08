d350543027c4c7603eb416af5f7b534e8245e297
Jakub Milkowski <jakub.milkowski@brival.co>
2024-12-08 15:18:48 +0100

    add install tailwind

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
-npm install rimraf
+npm install -D tailwindcss postcss autoprefixer svelte-preprocess npx
 python3.9 manage.py collectstatic
 python3.9 manage.py migrate
 python3.9 manage.py tailwind build
