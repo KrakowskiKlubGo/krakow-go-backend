470088ea8e7d9f857951c61e0d4872a072eb77cb
Jakub Milkowski <jakub.milkowski@brival.co>
2024-12-08 16:04:59 +0100

    install tailwind django

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
 pip install -r requirements.txt
-# update to node 20
-npm install -g npm@latest
-npm install -D rimraf cross-env@7.0.3
 python3.9 manage.py collectstatic
 python3.9 manage.py migrate
+python3.9 manage.py tailwind install
 python3.9 manage.py tailwind build
