7bdbc3eafb09afe18497d3bc0e476c203b635016
Jakub Milkowski <jakub.milkowski@brival.co>
2024-12-08 15:55:12 +0100

    udpate node

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 pip install -r requirements.txt
+# update to node 20
+npm install -g npm@latest
 npm install -D rimraf cross-env
-npm install
 python3.9 manage.py collectstatic
 python3.9 manage.py migrate
 python3.9 manage.py tailwind build
